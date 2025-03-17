fd3d864b2723be674e9c4ca1c38650f4541e54e1
V1A2M3 <98373394+V1A2M3@users.noreply.github.com>
2025-03-17 15:40:44 +0530

    Update model.py

--- ai_tutor/model.py
+++ ai_tutor/model.py
@@ -1,5 +1,6 @@
 from langchain_google_genai import ChatGoogleGenerativeAI
 
 def get_ai_response(user_input):
-    llm = ChatGoogleGenerativeAI(model="gemini-1.5-pro")
-    return llm.predict(user_input)
+    llm = ChatGoogleGenerativeAI(model="gemini-1.5-pro", google_api_key="AIzaSyAPECPvgOQcYhZ4-Ch-mt17y4f4Xax7u4I")
+    return llm.invoke(user_input)
+
