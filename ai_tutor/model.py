a14eb11a6f54a77a7d5b9ff996b83a5ab705d439
V1A2M3 <98373394+V1A2M3@users.noreply.github.com>
2025-03-17 16:17:19 +0530

    Update model.py

--- ai_tutor/model.py
+++ ai_tutor/model.py
@@ -1,11 +1,6 @@
-import os
 from langchain_google_genai import ChatGoogleGenerativeAI
 
 def get_ai_response(user_input):
-    api_key = os.getenv("GOOGLE_API_KEY")  
-    if not api_key:
-        raise ValueError("Missing Google API Key. Set GOOGLE_API_KEY as an environment variable.")
-    
-    llm = ChatGoogleGenerativeAI(model="gemini-1.5-pro", google_api_key="AIzaSyAPECPvgOQcYhZ4-Ch-mt17y4f4Xax7u4I", streaming=True)
-    
+    llm = ChatGoogleGenerativeAI(model="gemini-1.5-pro", google_api_key="AIzaSyAPECPvgOQcYhZ4-Ch-mt17y4f4Xax7u4I")
     return llm.invoke(user_input)
+
